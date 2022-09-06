446a917584314c4e2d83edbe622a0b6a659ab143
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-09-06 22:45:57 +0700

    Casting Java
    Output yang dihasilkan akan di casting kedalam bentuk integer

---
+++ Casting.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    public static void main(String args[]) {
+      
+      int myInt;
+      double myPercent = 51.8;
+      myInt = (int)myPercent;
+	    System.out.println(myInt);
+
+    }
+}

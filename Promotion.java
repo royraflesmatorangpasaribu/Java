73767ffc99d645a9d1ee28d998dc470d7ddb150b
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-09-06 22:41:24 +0700

    Promotion
    Promosi tipe data dari integer ke double

---
+++ Promotion.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static void main(String args[]) {
+      
+      int num1 = 55555;
+      int num2 = 77777;
+      double num3;
+      num3 = (double)num1 * num2;
+	    System.out.println(num3);
+
+    }
+}

2c8481b4d46b3679f002acb804cf376fbd0e5609
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-10-03 22:01:42 +0700

    Create Abstract.java

---
+++ Abstract.java
@@ -0,0 +1,45 @@
+//abstrak class
+//blue print suatu class
+abstract class Hewan{
+    //abstract method
+    public abstract void Suara(); //jika tidak menggunakan abstract dia akan menjadi method biasa
+    public abstract void Makan();
+    
+}
+//mengovewrite method yang di atas
+class Kucing extends Hewan{
+    @Override
+    public void Suara(){
+        System.out.println("Kucing : meong... meong");
+    }
+    @Override
+    public void Makan(){
+        System.out.println("Kucing makan ikan");
+    }
+}
+
+
+class Anjing extends Hewan{
+    @Override
+    public void Suara(){
+        System.out.println("Anjing : gong... gong...");
+    }
+    @Override
+    public void Makan(){
+        System.out.println("Anjing makan ikan");
+    }
+    
+}
+
+public class KelompokAbstract {
+    public static void main(String[] SalsaBilaJuu){
+        Kucing Salju = new Kucing();
+        Anjing Snowman = new Anjing();
+    
+        Salju.Suara();
+        Salju.Makan();
+        Snowman.Suara();
+        Snowman.Makan(); 
+    }
+    
+}

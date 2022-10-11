eb12d0ff8ba51c6627a56c9b763865435706a44a
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-10-11 23:58:20 +0700

    Create Promosi.java

---
+++ Promosi.java
@@ -0,0 +1,11 @@
+public class Promosi {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num1 = 55555;
+        int num2 = 55555;
+        long result = (long)num1*num2;
+        
+        System.out.println(result);
+    }
+    
+}

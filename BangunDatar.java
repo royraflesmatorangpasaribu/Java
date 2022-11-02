b4f05b9078de6e994a5bf11b8849b8fa385fd002
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-11-02 10:46:11 +0700

    Superclass

---
+++ BangunDatar.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum10_roy;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class BangunDatar {
+    private String warna;
+    
+    public BangunDatar(String warna){
+        this.warna = warna;
+    }
+    public BangunDatar(){
+        
+    }
+    
+    public String getWarna(){
+        return warna;
+    }
+    
+    public void setWarna(String warna){
+        this.warna = warna;
+    }
+    
+    public double luas(){
+        return 0;
+    }
+    
+    public double keliling(){
+        return 0;
+    }
+    
+}

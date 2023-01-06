554b5fdd13f77de4efcf32ca3fc06d3d85389600
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2023-01-06 22:48:00 +0700

    Update SearchProgram.java

--- SearchProgram.java
+++ SearchProgram.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package searchprogram.java;
+package no1.java;
 
 import java.util.Scanner;
 
@@ -11,7 +11,7 @@ import java.util.Scanner;
  *
  * @author ASUS
  */
-public class SearchProgram {
+public class No1Java {
 
     /**
      * @param args the command line arguments
@@ -20,16 +20,21 @@ public class SearchProgram {
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
         int n, cari;
+        System.out.print("Masukkan n Bilangan : ");
         n = input.nextInt();
         int[] a = new int[n];
         boolean cek = false;
         
         for(int i = 0; i < n; i++){
+            System.out.print("Masukkan Bilangan ke-" + i + " : ") ;
             a[i] = input.nextInt();
         }
         
+        System.out.print("Bilangan yang ingin di cari : ");
         cari = input.nextInt();
         
+        
+        System.out.println("\n Output:");
         for(int i = 0; i < n; i++){
             if(cari == a[i]){
                 System.out.println("Nilai ditemukan");

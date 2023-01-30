a8151c658a74892ad879b593613080b49585e4d3
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2023-01-30 16:53:54 +0700

    Program

---
+++ DiagramClass5/DiagramClass.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+import java.time.Month;
+
+class Person {
+    private String name;
+    private LocalDate dateOfBirth;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+}

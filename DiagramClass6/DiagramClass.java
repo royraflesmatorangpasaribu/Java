bfa5b97afd0aa8bb5a9b28bc8932f34d05cb2a9f
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2023-01-31 23:14:33 +0700

    Update DiagramClass.java

--- DiagramClass6/DiagramClass.java
+++ DiagramClass6/DiagramClass.java
@@ -1,2 +1,28 @@
 import java.time.LocalDate;
 import java.time.Month;
+
+class Person {
+    private String name;
+    private LocalDate dateOfBirth;
+
+    public Person(String name, LocalDate dateOfBirth) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+}

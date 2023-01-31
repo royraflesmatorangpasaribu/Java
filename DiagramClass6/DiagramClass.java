f684ae8d6bc1b94262e77df589345bc571f52ac8
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2023-01-31 23:14:47 +0700

    Update DiagramClass.java

--- DiagramClass6/DiagramClass.java
+++ DiagramClass6/DiagramClass.java
@@ -26,3 +26,29 @@ class Person {
         return dateOfBirth;
     }
 }
+
+class Student extends Person{
+    private String npm;
+    private String programStudi;
+
+    public Student(String npm, String programStudi, String name, LocalDate dateOfBirth) {
+        super(name, dateOfBirth);
+        this.npm = npm;
+        this.programStudi = programStudi;
+    }
+    
+    public String getProgramStudi() {
+        return programStudi;
+    }
+    public void setProgramStudi(String programStudi) {
+        this.programStudi = programStudi;
+    } 
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+}

a17fadd36b89f8152e3d6c062ea1bcd42c978998
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2023-02-01 23:28:43 +0700

    Update DiagramClass.java

--- DiagramClass7/DiagramClass.java
+++ DiagramClass7/DiagramClass.java
@@ -30,3 +30,30 @@ abstract class Person implements PersonInterface{
         return dateOfBirth;
     }  
 }
+
+class Student extends Person{
+    private String npm;
+    private String programStudi;
+
+    public Student(String npm, String programStudi, String name, LocalDate dateOfBirth) {
+        super(name, dateOfBirth);
+        this.npm = npm;
+        this.programStudi = programStudi;
+    }
+    
+    public String getProgramStudi() {
+        return programStudi;
+    }
+    
+    public void setProgramStudi(String programStudi) {
+        this.programStudi = programStudi;
+    } 
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+}

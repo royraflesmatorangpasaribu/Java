8257b3a3637942f513e52e49c37fa234381a7a06
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2023-02-01 23:28:11 +0700

    abstrack

--- DiagramClass7/DiagramClass.java
+++ DiagramClass7/DiagramClass.java
@@ -4,3 +4,29 @@ import java.time.Month;
 interface PersonInterface {
     public void menulis();
 }
+
+abstract class Person implements PersonInterface{
+    private String name;
+    private LocalDate dateOfBirth;
+
+    public Person(String name, LocalDate dateOfBirth) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }  
+}
